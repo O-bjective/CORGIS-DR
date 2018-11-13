48400c504d90beeb021be7941cf18811a73a7f34
jaryd <jarydchandos.5@gmail.com>
2018-11-13 10:28:31 -0800

    jaryd

---
+++ webapp.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, Markup, render_template, flash
+import os
+import json
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+     return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
