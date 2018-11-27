c7b35d36d398d2613dc089730538f1543b885f84
jaryd <jarydchandos.5@gmail.com>
2018-11-27 10:14:27 -0800

    J

--- webapp.py
+++ webapp.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 def render_main():
      return render_template('index.html')
 
-@app.route("Page1")
-def render_main():
-     return render_template('Page1.html')
+#@app.route("Page1")
+#def render_main():
+#     return render_template('Page1.html')
 
-@app.route("Page2")
-def render_main():
-     return render_template('Page2.html')
+#@app.route("Page2")
+#def render_main():
+#     return render_template('Page2.html')
 
 
 if __name__ == "__main__":
